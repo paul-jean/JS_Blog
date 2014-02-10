ad3fe9c01c172d4bd0b5487f064c8a3aa078526c
mirkok <mirko.klukas@gmail.com>
2014-02-10 16:30:11 -0500

    Initial commit

---
+++ test.js
@@ -0,0 +1,19 @@
+console.log("test");
+
+function Post(o) {
+	this.date = o.date;
+	this.content = o.content;
+	this.author = o.author;
+
+	return 99;
+}
+/*
+function Post(a,b,c) {
+	this.date = a;
+	this.content = b;
+	this.author = c;
+}
+*/
+
+x = { date : "10.02.2014", content: "" }
+
